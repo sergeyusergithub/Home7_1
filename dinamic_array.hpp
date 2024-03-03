0da7e0685b58042d5c531052f005be7623aecc4d
sergeyusergithub <sergeymailnew@mail.ru>
2024-03-03 12:40:52 +0500

    First commit

---
+++ dinamic_array.hpp
@@ -0,0 +1,196 @@
+﻿// dinamic_array.hpp
+
+/*!
+	\file 
+	\brief Описание задачи
+
+	\details
+Обязятельными пунктаи задания являются следующие возможности коллекции:
+-- размещение в себе элементов произвольного типа;
+-- добавление и удаление элементов;
+-- предоставление доступа к элементам массива;
+-- возможность работы с колекцией через range based for, sdt::size;
+-- корректная передача по параметру в функции.
+
+*/
+
+#pragma once
+
+#ifndef DINAMIC_ARRAY_HPP
+#define DINAMIC_ARRAY_HPP
+#include <iostream>
+
+
+/**
+ * @brief Шаблонный класс динамический массив 
+ * \author Буньков Сергей
+ * \version 1.0.0
+ * \date 29.02.2024 
+ * @details Создан в учебныйх целях. Домашнее задание 7.
+ * @tparam Type - параметр шаблона
+ */
+template<typename Type>
+class DinamicArray {
+public:
+
+	/// конструктор по умолчанию
+	DinamicArray() :data_{}, size_{} {}
+
+	/// @brief конструктор с параметром, параметр - размер динамического массива
+	/// @param size - размер создаваемого массива
+	DinamicArray(int size) :data_{}, size_(size) {
+		data_ = new Type[size_]{};
+	}
+
+	/**
+	 * @brief конструктор копирования
+	 * @param other - ссылка на другой динамический массив  
+	 * 
+	 */
+	DinamicArray(const DinamicArray<Type>& other);
+
+	DinamicArray<Type>& operator =(const DinamicArray<Type>& other);
+
+	DinamicArray<Type>& operator =(const char* str);
+
+	/// @brief оператор [] доступ к элементам массива
+	/// @param index - индекс элемента массива
+	/// @return Type& - возвращается ссылка на содержимое массива
+	/// которое может быть изменено
+	Type& operator[](int index) noexcept;
+
+	/**
+	 * @brief оператор [] доступа к содержимому массива
+	 * 
+	 * @param index - индекс элемента массива 
+	 * @return const Type& - возвращается константная ссылка на содержимео
+	 * 			позволяя получить содержимое массива
+	 */
+	const Type& operator[](int index) const noexcept;
+		
+	/// @brief деструктор, освобождает память
+	~DinamicArray() {
+		delete[] data_;
+		size_ = {};
+	}
+
+	/// @brief метод возвращающий размер массива, также необходим для использования функцией std::size()
+	int size() const {
+		return size_;
+	}
+
+	/**
+	 * @brief метод добавления элемента к концу 
+	 * массива, при этом массив увеличивает свой 
+	 * размер на добавленный элемент
+	 * 
+	 * @param data - данные добавляемы в конец массива
+	 */
+	void add(const Type& data);
+
+	/// @brief метод удаления элемента с конца массива,
+	/// при этом массив уменньшает свой массив на удаленный элемент
+	void remove();
+
+	/// @brief возвращает адрес начала массива
+	/// @return Type* - возвращает адрес заданного типа
+	Type* begin() {
+		return data_;
+	}
+
+
+	
+	/// @brief возвращает адрес конца массива
+	/// @return Type* - возвращает адрес заданного типа
+	Type* end() {
+		return data_ + size_;
+	}
+
+
+private:
+	Type *data_;
+	int size_;
+};
+
+
+//===================================================================
+template<typename Type>
+DinamicArray<Type>::DinamicArray(const DinamicArray<Type>& other)
+{
+	size_ = other.size_;
+	data_ = new Type[size_]{};
+	for (int i = 0; i < size_; ++i) {
+		data_[i] = other.data_[i];
+	}
+}
+
+
+
+//! ===================================================================
+template<typename Type>
+Type& DinamicArray<Type>::operator[](int index) noexcept
+{
+	return data_[index];
+}
+
+
+//==============================================================
+template<typename Type>
+const Type& DinamicArray<Type>::operator[](int index) const noexcept
+{
+	return data_[index];
+}
+
+template<typename Type>
+inline void DinamicArray<Type>::add(const Type& data)
+{
+	Type* tmp = new Type[size_ + 1];
+	for (int i = 0; i < size_; ++i) {
+		tmp[i] = data_[i];
+	}
+	delete[] data_;
+	data_ = tmp;
+	data_[size_] = data;
+	size_ += 1;
+}
+
+template<typename Type>
+inline void DinamicArray<Type>::remove()
+{
+	--size_;
+	Type* tmp = new Type[size_]{};
+	for (int i = 0; i < size_; ++i) {
+		tmp[i] = data_[i];
+	}
+	delete[] data_;
+	data_ = tmp;
+	
+}
+
+
+template<typename Type>
+DinamicArray<Type>& DinamicArray<Type>::operator =(const DinamicArray<Type>& other){
+	size_ = other.size_;
+	data_ = new Type[size_]{};
+	for (int i = 0; i < size_; ++i) {
+		data_[i] = other.data_[i];
+	}
+	return *this;
+}
+
+template<typename Type>
+DinamicArray<Type>& DinamicArray<Type>::operator =(const char* str){
+	int count {};
+	while(str[count] != '\0'){
+		if(count < size_){
+			data_[count] = str[count];
+		} else {
+			add(str[count]);
+		}
+		
+		count++;
+	}
+	return *this;
+}
+
+#endif // !DINAMIC_ARRAY_HPP
---
+++ main.cpp
@@ -0,0 +1,120 @@
+﻿// main.cpp
+
+#include <iostream>
+#include "dinamic_array.hpp"
+
+/// @brief  функция заполнение динамического массива
+/// типа char строкой символов не увеличивая размер массива
+/// @param str - динамический массив типа char
+/// @param chr - константная строка
+void arr_char(DinamicArray<char>& str, const char* chr) {
+	for (int i = 0; i < str.size(); i++) {
+		str[i] = chr[i];
+	}
+}
+
+int main() {
+
+	// создание массива типа int определенного размера
+	DinamicArray<int> arr(10);
+
+	// заполним произвольную ячейку массива числом
+	std::cout << "Enter the data into array and check it: ";
+	arr[3] = 15;
+
+	// выведем результат и посмотрим соответсвует ли введенному значению
+	std::cout << arr[3] << std::endl;
+	
+	// проверим текущий размер массива
+	std::cout << "Check the size of array with std::size() = " << std::size(arr) << '\n';
+
+	// добавим еще число в конец массива, данная операция возможна
+	// так как массив может расширяться
+	arr.add(12);
+
+	// проверим, что действительно массив стал больше, выведем размер массива
+	std::cout << "Check the new size of array with std::size() = " << std::size(arr) << '\n';
+
+	// проверим содержимое конца массива, чтобы узнать добавилось ли наше число
+	std::cout << "Print last two value of the new array:\n";
+	std::cout << "arr[10] = " << arr[10] << '\n';
+	std::cout << "arr[9] = " << arr[9] << '\n';
+
+	// удалим последний элемент массива
+	std::cout << "Remove last data.\n";
+	arr.remove();
+
+	//проверим снова размер массива после удаления элемента массива 
+	std::cout << "Check one more time the size of array  with std::size() = " << std::size(arr) << '\n';
+
+	// проверка работы с range base for циклом
+	// выведем содержимое массива
+	std::cout << "Print contents of array using range base for:\n";
+	for (auto &ar : arr) {
+		std::cout << ar << ' ';
+	}
+	std::cout << "\n";
+
+	// добавим еще данных в массив
+	std::cout << "Add new data to array.\n";
+	arr.add(9);
+
+	
+	// выведем содержимое массива range base for циклом
+	std::cout << "Print new contents of array using range base for:\n";
+	for (auto& ar : arr) {
+		std::cout << ar << ' ';
+	}
+
+	// создаем массив символов длинной 10
+	DinamicArray<char> str(10);
+
+	std::cout << std::endl;
+
+	// заполняем массив строкой символов
+	str = "HelloWorld DDD";
+
+	
+	// выведем содержимое массива range base for циклом
+	for (auto& st : str) {
+		std::cout << st;
+	}
+
+	// создадим массив символов используя конструктор копирования
+	DinamicArray<char> str2(str);
+	DinamicArray<char> str3;
+	std::cout << std::endl;
+
+	// добавим к новой строке отличительный символ
+	str2.add('2');
+
+	// выведем содержимое массива range base for циклом
+	for (auto& st : str2) {
+		std::cout << st;
+	}
+	std::cout << std::endl;
+
+	// передача массива в функцию, для заполнения
+	// массива строкой
+	arr_char(str, "HelloWorld!!!!!!!!");
+
+	// выведем содержимое массива range base for циклом
+	for (auto& st : str) {
+		std::cout << st;
+	}
+	std::cout << '\n';
+
+	//проверим работу оператора присваивания копированием
+	str3 = str;
+
+	str3.add('N');
+
+	// выведем содержимое массива range base for циклом
+	for (auto& st : str3) {
+		std::cout << st;
+	}
+
+	std::cout << "\n\n";
+
+	return 0;
+}
